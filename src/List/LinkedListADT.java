5ae11123dd7add6bc363cdfe20b786421e015adc
Kapil Kaushik <Kapilkaushik9143@gmail.com>
2020-10-20 12:16:29 +0530

    add remaining methods of Linked List

--- src/List/LinkedListADT.java
+++ src/List/LinkedListADT.java
@@ -1,19 +1,19 @@
 package List;
 
 public interface LinkedListADT<E> extends Iterable {
-    public boolean isEmpty();
-    public  int size();
-    public E last();
-    public E first();
-    public void addFirst(E e);
-    public void addLast(E e);
-    public void add(E e);
-    public void add(int index , E e) throws IllegalArgumentException;
-    public E removeFirst() throws IllegalAccessException;
-    public E removeLast() throws IllegalArgumentException;
-    public E remove() throws IllegalArgumentException;
-    public E remove(int index) throws IllegalArgumentException;
-    public void printAll();
-    public E get(int index) throws IllegalArgumentException;
+    boolean isEmpty();
+    int size();
+    E last();
+    E first();
+    void addFirst(E e);
+    void addLast(E e);
+    void add(E e);
+    void add(int index , E e) throws IllegalArgumentException;
+    E removeFirst() throws IllegalAccessException;
+    E removeLast() throws IllegalArgumentException;
+    E remove() throws IllegalArgumentException;
+    E remove(int index) throws IllegalArgumentException;
+    void printAll();
+    E get(int index) throws IllegalArgumentException;
 
 }
